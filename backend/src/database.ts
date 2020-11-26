32b9103ade1d0781ae23239241f8d41ac08e4084
Rafael Moura <71967181+rafasysop@users.noreply.github.com>
2020-11-26 09:49:57 -0300

    Update database.ts

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize';
 
-const sequelize = new Sequelize('mysql://root:baralh071@localhost:3306/pitu');
+const sequelize = new Sequelize('mysql://root:SUASENHA@localhost:3306/pitu');
 
 
 export default sequelize;
